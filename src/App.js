2492e41474e22566cb3208bc71af7ae1bdd4847e
Salvation27 <nikolayzalipykin@gmail.com>
2021-11-21 11:48:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import {v4 as uuidv4} from 'uuid'
+import {randomColor} from 'randomcolor'
+// import Draggable from "react-draggable";
+
+import {fetchWeather} from './api/fetchWaether'
+
 
 function App() {
+
+  const [item,setItem] = useState('')
+  const [items,setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+  useEffect(()=>{
+    localStorage.setItem('items',JSON.stringify(items))
+  },[items])
+
+ 
+const deleteItem = (id)=> {
+ setItems(items.filter(item=>item.id !== id))
+}
+
+// const keyPres = (e) => {
+//   const code = e.keyCode || e.which
+//   if(code === 13) {
+//    newItem()
+//   }
+// }
+
+const [query,setQuery]= useState('')
+const [weather,setWeather]= useState({})
+// const [color,setColor] = useState(false)
+
+const serch = async ()=>{
+  // if(e.key==='Enter'){
+    const data = await fetchWeather(query)
+    setWeather(data)
+    console.log(data)
+    // if(data.cod === 200){
+    //   setColor(true)
+    // }
+    const {main,name} = data
+     if (data) {
+
+       const newItem = {
+         id: uuidv4(),
+         main: main,
+         code: data.cod,
+         name: name,
+         color: randomColor({
+           luminosity: "light",
+         }),
+       };
+       setItems((items) => [...items, newItem]);
+      //  setItem("");
+     } else {
+       alert("Поле ввода пустое");
+      //  setItem("");
+     }
+    setQuery('')
+  // }
+}
+
+ const newItem = () => {
+   if (item.trim() !== "") {
+     const newItem = {
+       id: uuidv4(),
+       item: item,
+       color: randomColor({
+         luminosity: "light",
+       }),
+     };
+     setItems((items) => [...items, newItem]);
+     setItem("");
+   } else {
+     alert("Поле ввода пустое");
+     setItem("");
+   }
+ };
+
+//  color === true ? 'green' : 'red'
+// console.log(color)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        {/* <input
+          value={item}
+          type="text"
+          placeholder="введите текст"
+          // onKeyPress={(e) => keyPres(e)}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button className="enter" onClick={newItem}>
+          Создать
+        </button> */}
+        <div className="wrapper_wether">
+          <h1>Погода</h1>
+          <input
+            type="text"
+            className="serch"
+            placeholder="введите страну"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            // onKeyPress={serch}
+          />
+          <button onClick={serch}>Load</button>
+          <span>{query}</span>
+          {/* <span className={`red `}>rrrrr</span> */}
+          {weather.main && (
+            <div className="city">
+              <h2 className="city_name">
+                Найдена страна:
+                <span>{weather.name}</span>
+                {/* <sup>{weather.sys.country}</sup> */}
+              </h2>
+              {/* <div className="city_temp">{weather.main.temp}</div> */}
+            </div>
+          )}
+        </div>
+        <div className="wrapper_list">
+          {items.map((item, i) => {
+            return (
+              <div
+                className="todoo_item"
+                key={i}
+                style={{ backgroundColor: item.color }}
+              >
+                {item.name}
+                {item.main.temp}
+                {' '}
+                 {item.main.humidity}
+                <button onClick={() => deleteItem(item.id)}>Удалить</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/api/fetchWaether.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather"; 
+// const API_key = "2daede86c6d3922180bb9aeeb97717eb";
+const API_key = "8d25ed1420f8f1a60e8c585cf60c7834";
+
+export const fetchWeather = async (query) => {
+ const { data } = await axios.get(URL, {
+   params: {
+     q: query,
+     units: 'metric',
+     appid: API_key,
+   }
+ });
+ return data
+}
